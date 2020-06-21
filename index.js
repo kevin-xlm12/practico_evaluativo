76497b1b36e17fbc98c503bef8b4ea2aeaa43a65
kevin-xlm12 <63168592+kevin-xlm12@users.noreply.github.com>
2020-06-21 14:18:34 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const Perro= require('./Perro');
+pr=new Perro(4,"pelo grueso,cantidad moderada","chico");
+console.info(pr.expesarse());
+const Gato=require('./Gato');
+pr=new Gato(4,"pelo fino,cantidad moderada","nose");
+console.info(pr.expesarse());
+const Lobo= require('./Lobo');
+pr=new Lobo(4,"pelo fino,mucha cantidad","bosque");
+console.info(pr.expesarse())
